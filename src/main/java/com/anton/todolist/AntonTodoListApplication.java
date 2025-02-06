f8544b1874d94221eb820b667c82351becaa4817
Alessandro Menezes <alessandromenezes22@gmail.com>
2025-02-05 23:15:12 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/anton/todolist/AntonTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.anton.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntonTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntonTodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anton/todolist/AntonTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anton.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntonTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
